101d370ebffd22f1f2ae63347c1d25deacd72368
Buddy Delaune <BCDel89@gmail.com>
2020-12-07 23:24:55 -0600

    prod ready scraper.. maybe

--- README.md
+++ README.md
@@ -26,9 +26,25 @@ Install the project's dependencies
 npm install
 ```
 
+## Usage Options
+The following variables in the config.js can be configured before startup
+
+```js
+// How many sites to attempt to process at once can be an integer between 1 - 1000
+this.batchSize = 25;
+
+// Time IN SECONDS to wait between batches
+this.throttle = 1;
+
+// Restrict sites to a specific location, options are: [false, "US", "CAN", "UK", "EUR", "AUS"]
+// **NOTE** Temporarily RESTRICTED to US while we get the other locations setup
+this.country = "US";
+```
+
 ## Usage
 ```sh
 npm start
 ```
+
 
 MIT License. Copyright (c) 2020 Buddy C Delaune
--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 'use strict';
+const env = require('dotenv').config();
 
 module.exports = new function() {
+  this.production = process.env.NODE_ENV || true;
 
-  this.production = false;
+  // ----------------------------------------------------------------------
+	// v v v v v v v v v v v v CONFIGURABLE OPTIONS v v v v v v v v v v v v v
+  // ----------------------------------------------------------------------
 
   // How many sites to attempt to process at once can be an integer between 1 - 1000
   this.batchSize = 25;
@@ -13,4 +17,7 @@ module.exports = new function() {
   // Restrict sites to a specific location, options are: [false, "US", "CAN", "UK", "EUR", "AUS"]
   this.country = "US";
 
+  // ----------------------------------------------------------------------
+  // ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ CONFIGURABLE OPTIONS ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+  // ----------------------------------------------------------------------
 }();
