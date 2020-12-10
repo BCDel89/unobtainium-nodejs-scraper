a0143ef82a48c56167eb7ae638769cb180bcb5ab
Buddy Delaune <BCDel89@gmail.com>
2020-12-09 22:56:52 -0600

    formatting; no env protection

--- config.js
+++ config.js
@@ -1,25 +1,29 @@
 'use strict';
 const fs = require('fs');
 const env = require('dotenv');
-const tmpEnv = env.parse((fs.readFileSync('.env') || '').toString());
 
-if (tmpEnv.NODE_ENV === 'dev') env.config();
+try {
+	const tmpEnv = env.parse((fs.readFileSync('.env') || '').toString());
+	if (tmpEnv.NODE_ENV === 'dev') env.config();
+} catch (e) { }
 
-module.exports = new function() {
-  // ----------------------------------------------------------------------
+
+
+module.exports = new function () {
+	// ----------------------------------------------------------------------
 	// v v v v v v v v v v v v CONFIGURABLE OPTIONS v v v v v v v v v v v v v
-  // ----------------------------------------------------------------------
+	// ----------------------------------------------------------------------
 
-  // How many sites to attempt to process at once can be an integer between 1 - 1000
-  this.batchSize = 10;
+	// How many sites to attempt to process at once can be an integer between 1 - 1000
+	this.batchSize = 10;
 
-  // Time IN SECONDS to wait between batches
-  this.throttle = 2;
+	// Time IN SECONDS to wait between batches
+	this.throttle = 2;
 
-  // Restrict sites to a specific location, options are: [false, "US", "CAN", "UK", "EUR", "AUS"]
-  this.country = "US";
+	// Restrict sites to a specific location, options are: [false, "US", "CAN", "UK", "EUR", "AUS"]
+	this.country = "US";
 
-  // ----------------------------------------------------------------------
-  // ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ CONFIGURABLE OPTIONS ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
-  // ----------------------------------------------------------------------
+	// ----------------------------------------------------------------------
+	// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ CONFIGURABLE OPTIONS ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+	// ----------------------------------------------------------------------
 }();
