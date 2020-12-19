b17b7e73c21db8b9eb8e978ff9675c022cae7c0a
Buddy Delaune <BCDel89@gmail.com>
2020-12-19 16:18:31 -0600

    clear blacklist after 250 cycles; clear cached server files so that we retrieve latest crawler code

--- index.js
+++ index.js
@@ -6,18 +6,27 @@ const env = process.env.NODE_ENV || 'production';
 const shouldRequireFromServer = env === 'production';
 const apiUrl = process.env.API_URL || 'https://unobtainium.app/';
 const unobtainiumCrawlerUrl = apiUrl + 'public/unobtainiumCrawler.js';
+const blackListResetCycleLimit = 250;
 
 let blackList = [];
+let cycles = 0;
 
 (async () => {
 
 	const start = async () => {
 		try {
+			cycles += 1;
 			console.log('Starting Web Scraping Process with configuration:', config);
+			console.log('Beginning cycle: ', cycles);
+			if (cycles % 250 === 0) blackList = [];
+
+			const resolves = require('require-from-url/resolves');
+			Object.keys(resolves).forEach(k => delete resolves[k]);
 
 			const crawler = shouldRequireFromServer ?
 				requireFromUrl(unobtainiumCrawlerUrl) :
 				require('../unobtainium-api/scripts/unobtainiumCrawler');
+			// requireFromUrl(unobtainiumCrawlerUrl);
 
 			console.log('Process started, scraping..');
 
