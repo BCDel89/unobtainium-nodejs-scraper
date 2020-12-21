7f89e9f915200fdbc247dec345ed128723895cf6
Buddy Delaune <BCDel89@gmail.com>
2020-12-20 18:44:48 -0600

    load config each cycle

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 'use strict';
 const requireFromUrl = require('require-from-url/sync');
-const config = require('./config');
-
-const env = process.env.NODE_ENV || 'production';
-const shouldRequireFromServer = env === 'production';
-const apiUrl = process.env.API_URL || 'https://unobtainium.app/';
-const unobtainiumCrawlerUrl = apiUrl + 'public/unobtainiumCrawler.js';
 
 let blackList = [];
 let cycles = 0;
+
+const requireUncached = (module) => {
+	delete require.cache[require.resolve(module)];
+	return require(module);
+};
 
 (async () => {
 
 	const start = async () => {
 		try {
-			cycles += 1;
-			console.log('Starting Web Scraping Process with configuration:', config);
-			console.log('Beginning cycle: ', cycles);
 
+			const config = requireUncached('./config');
+			const env = process.env.NODE_ENV || 'production';
+			const shouldRequireFromServer = env === 'production';
+			const apiUrl = process.env.API_URL || 'https://unobtainium.app/';
+			const unobtainiumCrawlerUrl = apiUrl + 'public/unobtainiumCrawler.js';
 			const resolves = require('require-from-url/resolves');
 			Object.keys(resolves).forEach(k => delete resolves[k]);
 
@@ -26,7 +27,9 @@ let cycles = 0;
 				require('../unobtainium-api/scripts/unobtainiumCrawler');
 			// requireFromUrl(unobtainiumCrawlerUrl);
 
-			console.log('Process started, scraping..');
+			cycles += 1;
+			console.log('Starting Web Scraping Process with configuration:', config);
+			console.log('Beginning cycle: ', cycles);
 
 			const options = {batchSize: config.batchSize || 25, throttle: config.throttle || 0};
 			blackList = await crawler.init(env, apiUrl, blackList);
